156c96f4b38c2034f9bc6e06c9e4ffbf6d12c6e4
zemplab <124933999+zemplab@users.noreply.github.com>
2025-01-23 15:20:38 -0700

    core: zemp_bp.h agreed upon layout
    Also matlabs parser can't handle macros so we cannot use struct packing.

--- core/lib/zemp_bp.h
+++ core/lib/zemp_bp.h
@@ -1,30 +1,22 @@
 /* NOTE: standalone header for getting zemp_bp struct definition */
 #include <stdint.h>
 
-#ifdef _MSC_VER
-#define PACK(s) __pragma(pack(push, 1) ) s __pragma(pack(pop))
-#else
-#define PACK(s) s __attribute__((__packed__))
-#endif
-
-PACK(struct zemp_bp_v1 {
+typedef struct {
 	uint32_t version;
 	uint16_t decode_mode;
 	uint16_t beamform_mode;
 	uint32_t raw_data_dim[4];
 	uint32_t decoded_data_dim[4];
-	uint32_t transducer_count;
+	float    transducer_element_pitch[2];
+	float    transducer_transform_matrix[16]; /* NOTE: column major order */
+	int16_t  channel_mapping[256];
+	float    steering_angles[256];
+	float    focal_depths[256];
+	int16_t  sparse_elements[256];
+	int16_t  hadamard_rows[256];
 	float    speed_of_sound;
 	float    center_frequency;
 	float    sampling_frequency;
 	float    time_offset;
 	uint32_t transmit_mode;
-	float    transducer_element_pitch[2];
-	float    transducer_transform_matrices[64]; /* NOTE: column major order */
-	int16_t  channel_mapping[1024];
-	float    steering_angles[1024];
-	float    focal_depths[1024];
-	int16_t  sparse_elements[1024];
-	int16_t  hadamard_rows[1024];
-});
-typedef struct zemp_bp_v1 zemp_bp_v1;
+} zemp_bp_v1;
