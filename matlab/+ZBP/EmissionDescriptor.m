ca86dc99889dbaea84c5d6873c9017942e53df75
DarathDev <darath.dev@dolasoft.com>
2026-02-12 14:21:23 -0700

    matlab: make generated toBytes function non-static

--- matlab/+ZBP/EmissionDescriptor.m
+++ matlab/+ZBP/EmissionDescriptor.m
@@ -12,6 +12,20 @@ classdef EmissionDescriptor
 		byteSize(1,1) uint32 = 8
 	end
 
+	methods
+		function bytes = toBytes(obj)
+			arguments (Input)
+				obj(1,1) ZBP.EmissionDescriptor
+			end
+			arguments (Output)
+				bytes uint8
+			end
+			bytes = zeros(1, ZBP.EmissionDescriptor.byteSize);
+			bytes(1:4) = typecast(obj.emission_kind(:),     'uint8');
+			bytes(5:8) = typecast(obj.parameters_offset(:), 'uint8');
+		end
+	end
+
 	methods (Static)
 		function out = fromBytes(bytes)
 			arguments (Input)
@@ -23,18 +37,6 @@ classdef EmissionDescriptor
 			out = ZBP.EmissionDescriptor;
 			out.emission_kind(:)     = typecast(bytes(1:4), 'int32');
 			out.parameters_offset(:) = typecast(bytes(5:8), 'int32');
-		end
-
-		function bytes = toBytes(obj)
-			arguments (Input)
-				obj(1,1) ZBP.EmissionDescriptor
-			end
-			arguments (Output)
-				bytes uint8
-			end
-			bytes = zeros(1, ZBP.EmissionDescriptor.byteSize);
-			bytes(1:4) = typecast(obj.emission_kind(:),     'uint8');
-			bytes(5:8) = typecast(obj.parameters_offset(:), 'uint8');
 		end
 	end
 end
