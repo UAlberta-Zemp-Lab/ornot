5374a147f8779a3f5799590615b2ab54afdc216b
Darren Dahunsi <dahunsi@ualberta.ca>
2026-02-26 22:28:39 -0700

    matlab: use ZBP type for RCAOrientation

--- matlab/+ornot/unpackTransmitReceiveOrientation.m
+++ matlab/+ornot/unpackTransmitReceiveOrientation.m
@@ -1,23 +1,21 @@
 function [transmit_orientation, receive_orientation] = unpackTransmitReceiveOrientation(transmit_receive_orientation)
 %UNPACKTRANSMITRECEIVEORIENTATION Transmit Orientations are packed in the
 %top 4 bits of a uint8 while Receive Orientations are packed in the bottom
-%4 bits, with values matching the enum OGLBeamformerRCAOrientation
+%4 bits, with values matching the enum ZBP.RCAOrientation
 arguments (Input)
     transmit_receive_orientation uint8
 end
 arguments (Output)
-    transmit_orientation OGLBeamformerRCAOrientation
-    receive_orientation OGLBeamformerRCAOrientation
+    transmit_orientation ZBP.RCAOrientation
+    receive_orientation ZBP.RCAOrientation
 end
-transmit_orientation(numel(transmit_receive_orientation)) = OGLBeamformerRCAOrientation;
-receive_orientation(numel(transmit_receive_orientation)) = OGLBeamformerRCAOrientation;
-transmit_orientation = reshape(transmit_orientation, size(transmit_receive_orientation));
-receive_orientation = reshape(receive_orientation, size(transmit_receive_orientation));
+transmit_orientation = createArray(size(transmit_receive_orientation), "ZBP.RCAOrientation");
+receive_orientation = createArray(size(transmit_receive_orientation), "ZBP.RCAOrientation");
 for i = 1:numel(transmit_receive_orientation)
     transmit_receive_orientation(i) = ...
         bitshift(uint8(transmit_orientation), 4) ...
         + uint8(receive_orientation);
-    transmit_orientation(i) = OGLBeamformerRCAOrientation(bitand(bitshift(transmit_receive_orientation(i), -4), 15));
-    receive_orientation(i) = OGLBeamformerRCAOrientation(bitand(transmit_receive_orientation(i), 15));
+    transmit_orientation(i) = ZBP.RCAOrientation(bitand(bitshift(transmit_receive_orientation(i), -4), 15));
+    receive_orientation(i) = ZBP.RCAOrientation(bitand(transmit_receive_orientation(i), 15));
 end
 end
