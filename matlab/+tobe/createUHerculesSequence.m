860a0fb1ead04d47b0886bf0ddd547240b614017
DarathDev <darath.dev@dolasoft.com>
2026-08-20 15:17:30 -0600

    matlab: explicitly specify the expected dimensionality of extra parameters

--- matlab/+tobe/createUHerculesSequence.m
+++ matlab/+tobe/createUHerculesSequence.m
@@ -2,7 +2,7 @@ function [biasPattern, transmitApodization, transmitDelays, receiveApodization, 
 arguments (Input)
     array(1,1) tobe.RowColumnArray
     uHerculesParameters(:,1) ZBP.HERCULESParameters
-    sparseElements(:,:) uint16
+    sparseElements(:,:) uint16 % section_count x receive_event_count
     speedOfSound(1,1) single
 end
 arguments (Output)
@@ -88,5 +88,5 @@ beamformParameters.transducer_element_pitch = array.Pitch;
 beamformParameters.acquisition_kind = ZBP.AcquisitionKind.UHERCULES;
 beamformParameters.acquisition_parameters = uHerculesParameters;
 beamformParameters.time_offset = timeOffset;
-beamformParameters.sparse_elements = sparseElements - 1;
+beamformParameters.sparse_elements = (sparseElements - 1)';
 end
