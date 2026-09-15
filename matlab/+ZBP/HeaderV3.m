5719b79f74c58e0c5a05a8984233213d89a96db5
DarathDev <darath.dev@dolasoft.com>
2026-09-15 16:17:54 -0600

    zbp: move data_frame_delays to v3
    The change the ZBP header neccesitates a major version upgrade

--- matlab/+ZBP/HeaderV3.m
+++ matlab/+ZBP/HeaderV3.m
@@ -2,17 +2,17 @@
 
 % GENERATED CODE
 
-classdef HeaderV2
+classdef HeaderV3
 	properties
 		magic(1,1)                         uint64
 		major(1,1)                         uint32
 		minor(1,1)                         uint32
 		raw_data_dimension(1,4)            uint32
-		raw_data_kind(1,1)                 uint32 % ZBP.DataKind
+		raw_data_kind(1,1)                 int32
 		raw_data_offset(1,1)               int32
-		raw_data_compression_kind(1,1)     uint32 % ZBP.DataCompressionKind
-		decode_mode(1,1)                   uint32 % ZBP.DecodeMode
-		sampling_mode(1,1)                 uint32 % ZBP.SamplingMode
+		raw_data_compression_kind(1,1)     int32
+		decode_mode(1,1)                   int32
+		sampling_mode(1,1)                 int32
 		sampling_frequency(1,1)            single
 		demodulation_frequency(1,1)        single
 		speed_of_sound(1,1)                single
@@ -25,26 +25,27 @@ classdef HeaderV2
 		time_offset(1,1)                   single
 		group_acquisition_time(1,1)        single
 		ensemble_repetition_interval(1,1)  single
-		acquisition_mode(1,1)              uint32 % ZBP.AcquisitionKind
+		acquisition_mode(1,1)              int32
 		acquisition_parameters_offset(1,1) int32
-		contrast_mode(1,1)                 uint32 % ZBP.ContrastMode
+		contrast_mode(1,1)                 int32
 		contrast_parameters_offset(1,1)    int32
 		emission_descriptors_offset(1,1)   int32
+		data_frame_delays_offset(1,1)      int32
 	end
 
 	properties (Constant)
-		byteSize(1,1) uint32 = 184
+		byteSize(1,1) uint32 = 188
 	end
 
 	methods
 		function bytes = toBytes(obj)
 			arguments (Input)
-				obj(1,1) ZBP.HeaderV2
+				obj(1,1) ZBP.HeaderV3
 			end
 			arguments (Output)
 				bytes uint8
 			end
-			bytes = zeros(1, ZBP.HeaderV2.byteSize);
+			bytes = zeros(1, ZBP.HeaderV3.byteSize);
 			bytes(1:8)     = typecast(obj.magic(:),                         'uint8');
 			bytes(9:12)    = typecast(obj.major(:),                         'uint8');
 			bytes(13:16)   = typecast(obj.minor(:),                         'uint8');
@@ -71,6 +72,7 @@ classdef HeaderV2
 			bytes(173:176) = typecast(obj.contrast_mode(:),                 'uint8');
 			bytes(177:180) = typecast(obj.contrast_parameters_offset(:),    'uint8');
 			bytes(181:184) = typecast(obj.emission_descriptors_offset(:),   'uint8');
+			bytes(185:188) = typecast(obj.data_frame_delays_offset(:),      'uint8');
 		end
 	end
 
@@ -80,18 +82,18 @@ classdef HeaderV2
 				bytes uint8
 			end
 			arguments (Output)
-				out(1,1) ZBP.HeaderV2
+				out(1,1) ZBP.HeaderV3
 			end
-			out = ZBP.HeaderV2;
+			out = ZBP.HeaderV3;
 			out.magic(:)                         = typecast(bytes(1:8),     'uint64');
 			out.major(:)                         = typecast(bytes(9:12),    'uint32');
 			out.minor(:)                         = typecast(bytes(13:16),   'uint32');
 			out.raw_data_dimension(:)            = typecast(bytes(17:32),   'uint32');
-			out.raw_data_kind(:)                 = typecast(bytes(33:36),   'uint32');
+			out.raw_data_kind(:)                 = typecast(bytes(33:36),   'int32');
 			out.raw_data_offset(:)               = typecast(bytes(37:40),   'int32');
-			out.raw_data_compression_kind(:)     = typecast(bytes(41:44),   'uint32');
-			out.decode_mode(:)                   = typecast(bytes(45:48),   'uint32');
-			out.sampling_mode(:)                 = typecast(bytes(49:52),   'uint32');
+			out.raw_data_compression_kind(:)     = typecast(bytes(41:44),   'int32');
+			out.decode_mode(:)                   = typecast(bytes(45:48),   'int32');
+			out.sampling_mode(:)                 = typecast(bytes(49:52),   'int32');
 			out.sampling_frequency(:)            = typecast(bytes(53:56),   'single');
 			out.demodulation_frequency(:)        = typecast(bytes(57:60),   'single');
 			out.speed_of_sound(:)                = typecast(bytes(61:64),   'single');
@@ -104,11 +106,12 @@ classdef HeaderV2
 			out.time_offset(:)                   = typecast(bytes(153:156), 'single');
 			out.group_acquisition_time(:)        = typecast(bytes(157:160), 'single');
 			out.ensemble_repetition_interval(:)  = typecast(bytes(161:164), 'single');
-			out.acquisition_mode(:)              = typecast(bytes(165:168), 'uint32');
+			out.acquisition_mode(:)              = typecast(bytes(165:168), 'int32');
 			out.acquisition_parameters_offset(:) = typecast(bytes(169:172), 'int32');
-			out.contrast_mode(:)                 = typecast(bytes(173:176), 'uint32');
+			out.contrast_mode(:)                 = typecast(bytes(173:176), 'int32');
 			out.contrast_parameters_offset(:)    = typecast(bytes(177:180), 'int32');
 			out.emission_descriptors_offset(:)   = typecast(bytes(181:184), 'int32');
+			out.data_frame_delays_offset(:)      = typecast(bytes(185:188), 'int32');
 		end
 	end
 end
